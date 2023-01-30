4874c22d2667c8584625c8e3af6b0049a1e77069
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2023-01-29 19:31:13 -0500

    case

--- src/poo/iccr/Evento/Salon.java
+++ src/poo/iccr/Evento/Salon.java
@@ -19,26 +19,17 @@ public class Salon {
 
     public void asignarSalon(NumeroAsistentes numeroAsistentes) {
         int numeroAsistentesTotales = numeroAsistentes.calcularAsistentesTotales();
-        if (numeroAsistentesTotales > 15 && numeroAsistentesTotales <= 25) {
-            this.tipoSalon = PLATINO;
-        }
-        if (numeroAsistentesTotales >= 26 && numeroAsistentesTotales <= 45) {
-            this.tipoSalon = ESMERALDA;
-        }
-        if (numeroAsistentesTotales > 45) {
-            this.tipoSalon = DIAMANTE;
-        }
+        if (numeroAsistentesTotales > 15 && numeroAsistentesTotales <= 25) this.tipoSalon = PLATINO;
+        if (numeroAsistentesTotales >= 26 && numeroAsistentesTotales <= 45) this.tipoSalon = ESMERALDA;
+        if (numeroAsistentesTotales > 45) this.tipoSalon = DIAMANTE;
     }
 
     public void calcularValorSalon(){
-        if(this.tipoSalon.equals(PLATINO)){
-            this.valor = 350;
+
+        switch (this.tipoSalon){
+            case PLATINO -> this.valor = 350;
+            case DIAMANTE -> this.valor = 425;
+            case ESMERALDA -> this.valor = 500;
         }
-        if(this.tipoSalon.equals(ESMERALDA)){
-            this.valor = 425;
-        }
-        if(this.tipoSalon.equals(DIAMANTE)){
-            this.valor = 500;
-        }
-    }
+   }
 }
